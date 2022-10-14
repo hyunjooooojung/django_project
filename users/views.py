b20e533ad26e3dd48f51cef1fce9b88e636b71af
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-14 20:01:35 +0900

    2nd day complete

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
+from django.contrib.auth import authenticate, login as loginsession
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def signup(request):
@@ -13,9 +15,36 @@ def signup(request):
         passwordcheck = request.POST.get('passwordcheck')
         if password == passwordcheck:
             User.objects.create_user(username=username, password=password)
-            return HttpResponse("회원가입 완료")
+            return render(request, 'login.html')
         else:
             return HttpResponse("비밀번호 확인 틀렸습니다.")
     
     else:
         return HttpResponse("허용되지 않은 메소드입니다.")
+    
+    
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html/')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            loginsession(request, user)
+            # Redirect to a success page.ee
+            return redirect('users:user')
+        else:
+            # Redirect to a success page.
+            return HttpResponse("로그인 실패.")
+                        
+                        
+def user(request):
+    return HttpResponse(request.user)
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    context = {
+        'user': user
+    }
+    return render(request, 'profile.html', context)
