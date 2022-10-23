0c94769f63759ed6c0cabcf66b081a39d02c49af
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-23 20:10:15 +0900

    comment 기능 추가

--- community/urls.py
+++ community/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('create_article/', views.create_article, name="create-article"),
     path('<int:article_id>/update_article/', views.update_article, name="update_article"),
     path('<int:article_id>/delete_article/', views.delete_article, name="delete_article"),
+    path('<int:article_id>/create_comment/', views.create_comment, name="create_comment"),
+    path('<int:article_id>/delete_comment/<int:comment_id>', views.delete_comment, name="delete_comment"),
 ]
 
 
